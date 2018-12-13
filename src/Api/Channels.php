1329a81915857d65e5b0f8f990dd04051e52b1b9
Mikus Rozenbergs <mikus.rozenbergs@gmail.com>
2018-12-13 15:15:59 +0200

    updated composer

--- src/Api/Channels.php
+++ src/Api/Channels.php
@@ -12,12 +12,20 @@ use mixisLv\Reamaze\Params\Channels\RetrieveParams as ChannelRetrieveParams;
 
 class Channels extends BaseApi
 {
+    /**
+     * retrieve
+     *
+     * @param ChannelRetrieveParams $params
+     *
+     * @return \stdClass
+     * @throws \mixisLv\Reamaze\Exceptions\ApiException
+     * @see https://www.reamaze.com/api/get_channels
+     */
     public function retrieve(ChannelRetrieveParams $params)
     {
-        $page = max(1, (int)$params->page);
         $channel  = $params->channel && in_array($params->channel, ChannelRetrieveParams::channelFilters()) ? $params->channel : '';
         $slug  = $params->slug;
 
-        return $this->api->call('channels' . ($slug ? '/' . $slug : '') , 'GET', ['page' => $page, 'channel' => $channel ]);
+        return $this->api->call('channels' . ($slug ? '/' . $slug : '') , 'GET', ['page' => $params->page, 'channel' => $channel ]);
     }
 }
